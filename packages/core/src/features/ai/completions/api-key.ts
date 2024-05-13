48ee7dcf5fe1be57d8101af62e108008f444c977
jjaychen <31304335+jjaychen1e@users.noreply.github.com>
2024-05-13 12:01:02 +0800

    fix: correct library imports

--- packages/core/src/features/ai/completions/api-key.ts
+++ packages/core/src/features/ai/completions/api-key.ts
@@ -1,13 +1,12 @@
 import type { FastifyRequest } from 'fastify'
-import type { AIConfig, AIServiceConfig } from '../../../types/config/ai'
+import type { AIConfig, AIServiceConfig, User } from '@ru/shared'
 import { getStore } from '../../../utils/store.util'
-import type { User } from '../../../types'
 
 export function getApiKey(request: FastifyRequest, aiConfig: AIConfig | undefined, config: AIServiceConfig | undefined): string | undefined {
   const raycastRequestHeaders = request.headers
   const token = raycastRequestHeaders.authorization
   const users = getStore<User[]>('users') || []
-  const user = users.find(u => u.token === token)
+  const user = users.find(u => u.tokens?.includes(token ?? ''))
 
   const usersApiKeyCustomizationEnabled = config?.usersApiKeyCustomizationEnabled || aiConfig?.usersApiKeyCustomizationEnabled || false
 
